4e5a236271ff97be18f5bd5eaec7054bc1743723
arunmozhithevan <76176063+arunmozhithevan@users.noreply.github.com>
2022-01-29 17:05:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let url = "https://dog.ceo/api/breeds/image/random";
+let container = document.getElementById("container");
+let image = document.getElementById("image");
+let link = document.getElementById("link");
+let Dbtn = document.getElementById("Dbtn");
+function show(params) {
+  let api = fetch(url);
+
+  api
+    .then((res) => {
+      if (res.status != 200) throw "not found";
+      return res.json();
+    })
+    .then((data) => {
+      image.src = data.message;
+      link.href = data.message;
+    })
+    .catch((data) => {
+      console.error("404 Not found");
+    });
+}
+Dbtn.addEventListener("click", () => {
+  link.click();
+});
+container.addEventListener("mouseover", () => {
+  Dbtn.style.display = "flex";
+});
+container.addEventListener("mouseleave", () => {
+  Dbtn.style.display = "none";
+});
+container.addEventListener("touchstart", (event) => {
+    event.stopPropagation();
+  Dbtn.style.display = "flex";
+});
+document.body.addEventListener("touchstart", (event) => {
+  Dbtn.style.display = "none";
+});
